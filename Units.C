73c38af32c7b8c8e353c7dd0e53d8f919851cf95
bruceonyancha964-web <bruceonyancha964@gmail.com>
2025-09-22 16:04:29 +0300

    Add files via upload

---
+++ Units.C
@@ -0,0 +1,20 @@
+//simple program highlighting the units semester 1 year 1
+
+/*
+Bruce onyancha
+PA106/G/28827/25
+program to display units of semester 1 year 1
+*/
+
+#include<stdio.h> //preprocessor directive
+//main function 
+int main(){
+printf("1.Discrete Mathematics\n");
+printf("2.Mathematics for Science\n");
+printf("3.Fundamentals of ICT\n");
+printf("4.Computer systems and Organisation\n");
+printf("5.Communication Skills\n");
+printf("6.Introduction to programming and Problem solving\n");
+printf("7.Fundamentals of System Analysis & Design\n");
+return 0;
+}
