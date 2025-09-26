02bf2831c82197fae7aff893871e72d9ca9fcfe3
bruceonyancha964-web <bruceonyancha964@gmail.com>
2025-09-26 10:43:11 +0300

    Add files via upload

---
+++ Prompt.cpp
@@ -0,0 +1,26 @@
+//simple program to prompt user to enter id,height&bank balance
+/*
+Bruce Onyancha
+PA106/G/28827/25
+Program to prompt user to enter id,height&bank balance
+*/
+#include<stdio.h> 
+//main function
+int main(){
+
+int id;
+int height;
+int bank_balance;
+
+
+printf("Enter your id number");
+scanf("%d",& id);
+
+printf("Enter your height");
+scanf("%d",& height);
+
+printf("Enter your bank balance");
+scanf("%d",& bank_balance);
+
+return 0;
+}
