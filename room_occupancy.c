086a1c724b2e3c48d4548f9cc885ae67e6729ede
bruceonyancha964-web <bruceonyancha964@gmail.com>
2025-10-30 09:02:08 +0300

    Add files via upload

---
+++ room_occupancy.c
@@ -0,0 +1,39 @@
+/*
+Name:BRUCE ONYANCHA
+Reg No:PA106/G/28827/25
+*/
+
+#include <stdio.h>
+#include <time.h>
+
+int main() {
+    int occupancy[5][10];
+    int floor, room;
+    int occupied, vacant;
+
+
+    srand(time(NULL));
+
+    // Simulate random occupancy data (1 = occupied, 0 = vacant)
+    for (floor = 0; floor < 5; floor++) {
+        for (room = 0; room < 10; room++) {
+            occupancy[floor][room] = rand() % 2;
+        }
+    }
+
+    // Display the number of occupied and vacant rooms per floor
+    printf("Room Occupancy by Floor:\n");
+    for (floor = 0; floor < 5; floor++) {
+        occupied = 0;
+        vacant = 0;
+        for (room = 0; room < 10; room++) {
+            if (occupancy[floor][room] == 1)
+                occupied++;
+            else
+                vacant++;
+        }
+        printf("Floor %d: Occupied = %d, Vacant = %d\n", floor + 1, occupied, vacant);
+    }
+
+    return 0;
+}
